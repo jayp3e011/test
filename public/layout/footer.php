cf33100dcfda542ec8ca1bd7e5ca6dfe0bc90d1c
cbalamanatuic@gmail.com <clydeinwebdev@gmail.com>
2017-03-04 16:50:33 +0800

    fixed version

---
+++ public/layout/footer.php
@@ -0,0 +1,7 @@
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<script src="plugins/fastclick/fastclick.js"></script>
+<script src="plugins/sweetalert/sweetalert.min.js"></script>
+<script src="dist/js/app.min.js"></script>
+<script src="controllers/script.js"></script>
+<script src="dist/js/demo.js"></script>
